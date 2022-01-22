402e6ac0c15ddec1a71255dad5be2eeaef1948ee
BenC25 <bhcraighill@gmail.com>
2022-01-23 10:21:21 +1100

    Added Premake File

---
+++ premake5.lua
@@ -0,0 +1,30 @@
+project "assimp"
+	kind "StaticLib"
+	language "C++"
+    staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"include/**.h"
+		"include/**.hpp"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
